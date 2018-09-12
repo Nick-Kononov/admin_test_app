abc0338062f6dfebc6f22c4c647405577c817761
nkononov <nick.kononov.12@gmail.com>
2018-09-12 11:42:47 +0300

    refactor card for no user case

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,7 +11,7 @@ class Card extends React.Component {
       <div className="card user-card">
         <div className="card-body">
           <h5 className="card-title">
-            {username || 'no user set'}
+            {username || 'No user'}
           </h5>
           <h6 className="card-subtitle mb-2 text-muted">{email}</h6>
           <Skills userSkills={user_skills} />
